7790553723ffa3c15e2abcc68679fb581ed76d7f
bahdev223 <bahdev223@gmail.com>
2026-08-01 11:09:16 +0000

    Modernize learner registry page

--- participants/tests/test_views.py
+++ participants/tests/test_views.py
@@ -1,2 +1,55 @@
 """Tests for participants views."""
 
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from core.testing import souscrire_plan_complet
+from organisations.models import Organisation
+from participants.models import Participant
+
+
+class ParticipantIndexViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_superuser(
+            username="participant-index-admin",
+            email="participant-index@example.test",
+            password="test1234",
+        )
+        cls.organisation = Organisation.objects.create(
+            nom="Centre Participants",
+            slug="centre-participants",
+            email="contact@participants.test",
+            telephone="+22370000008",
+        )
+        souscrire_plan_complet(cls.organisation)
+        cls.participant = Participant.objects.create(
+            organisation=cls.organisation,
+            prenom="Fatoumata",
+            nom="Bah",
+            telephone="+22370000009",
+            email="fatoumata@example.test",
+            profession="Assistante",
+            entreprise="OHT Service",
+        )
+
+    def setUp(self):
+        self.client.force_login(self.user)
+
+    def test_index_renders_operational_register(self):
+        response = self.client.get("/o/centre-participants/participants/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Fatoumata Bah")
+        self.assertContains(response, "OHT Service")
+        self.assertContains(response, "Apprenants")
+        self.assertContains(response, "Inscriptions")
+
+    def test_index_can_filter_by_search(self):
+        response = self.client.get(
+            "/o/centre-participants/participants/",
+            {"q": "OHT"},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Fatoumata Bah")
--- participants/views.py
+++ participants/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.db import models
 from django.views.generic import CreateView, ListView
 
 from core.mixins import HtmxModalFormMixin, OrganisationScopedMixin
@@ -17,7 +18,50 @@ class ParticipantIndexView(OrganisationScopedMixin, LoginRequiredMixin, Permissi
     paginate_by = 25
 
     def get_queryset(self):
-        return super().get_queryset().order_by("nom", "prenom")
+        queryset = super().get_queryset().annotate(
+            inscriptions_count=models.Count("inscriptions", distinct=True),
+            total_paye_sum=models.Sum(
+                "inscriptions__paiements__montant",
+                filter=models.Q(inscriptions__paiements__statut="VALIDE"),
+            ),
+        )
+        search = self.request.GET.get("q", "").strip()
+        statut = self.request.GET.get("statut", "").strip()
+        if search:
+            queryset = queryset.filter(
+                models.Q(matricule__icontains=search)
+                | models.Q(nom__icontains=search)
+                | models.Q(prenom__icontains=search)
+                | models.Q(telephone__icontains=search)
+                | models.Q(email__icontains=search)
+                | models.Q(profession__icontains=search)
+                | models.Q(entreprise__icontains=search)
+            )
+        if statut:
+            queryset = queryset.filter(statut=statut)
+        return queryset.order_by("nom", "prenom")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        base_qs = self.get_queryset()
+        context.update(
+            {
+                "search_query": self.request.GET.get("q", "").strip(),
+                "active_statut": self.request.GET.get("statut", "").strip(),
+                "statut_choices": Participant.Statut.choices,
+                "participants_total": base_qs.count(),
+                "participants_actifs": base_qs.filter(
+                    statut=Participant.Statut.ACTIF,
+                ).count(),
+                "participants_entreprises": base_qs.exclude(
+                    entreprise="",
+                ).count(),
+                "participants_inscriptions": sum(
+                    participant.inscriptions_count for participant in base_qs
+                ),
+            }
+        )
+        return context
 
 
 class ParticipantCreateView(OrganisationScopedMixin, HtmxModalFormMixin, LoginRequiredMixin, PermissionRequiredMixin, SuccessMessageMixin, CreateView):
